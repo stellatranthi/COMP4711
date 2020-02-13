dda67703176ca6ae71086e209fae915e1d2a212e
Stella Tran <stellatranthi@hotmail.com>
2020-02-13 13:59:25 -0800

    Testing

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-const express = require("express");
-const path = require('path');
-const fs = require("fs");
-const bodyParser = require('body-parser');
-const app = express();
-const util = require('util');
-const readFile = util.promisify(fs.readFile);
-const filePath = express.static(path.join(__dirname,'data.json'));
-
-app.use(staticPath);
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,'views','index.html'));
-})
+var express = require("express");
+var port = process.env.PORT || 3000;
+var app = express();
+app.get('/', function (req, res) {
+ res.send(JSON.stringify({ Hello: "World"}));
+});
